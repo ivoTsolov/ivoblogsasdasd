84096959ae55f8de707ae361e07280b099ff3885
ivoTsolov <ivailo.tsolov1990@gmail.com>
2018-07-03 15:41:54 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,120 @@
+
+
+const express = require('express');
+const app = express();
+//Import the mongoose module
+const mongoose = require('mongoose');
+// Import the body parser
+const bodyParser = require('body-parser')
+//Set up default mongoose connection
+const mongoDB = 'mongodb://127.0.0.1/posts';
+//cors 
+const cors = require('cors');
+
+mongoose.connect(mongoDB);
+// Get Mongoose to use the global promise library
+mongoose.Promise = global.Promise;
+//Get the default connection
+const db = mongoose.connection;
+
+const Schema = mongoose.Schema;
+
+
+const postSchema = new Schema({
+   post: String
+});
+
+const usersSchema = new Schema({
+  username: String,
+  password: String
+})
+const PostModel = mongoose.model("PostModel", postSchema);
+const UserModel = mongoose.model("UserModel", usersSchema);
+// using additional libraries
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
+//
+
+//post request
+app.post('/makeApost', createPost);
+
+function createPost(req, res){
+  let post= req.body;
+  console.log(post);
+  PostModel.create(post).then(
+  function(postObj){
+    console.log(postObj);
+    res.sendStatus(200);
+  },
+    function(err){
+    res.sendStatus(400);
+      
+    }
+  );
+}
+
+//
+
+//Get all posts
+app.get('/getAllPosts', getAllPosts);
+
+function getAllPosts(req, res){
+    PostModel.find({}).then((posts)=>{
+      res.json(posts);
+
+    },
+    (err)=>{
+      res.sendStatus(400)
+    }
+  )
+}
+//Edit a post
+
+app.put('/editApost', editApost);
+
+function editApost(req, res) {
+  const { _id: id, ...post } = req.body;
+  PostModel.findByIdAndUpdate(id, { $set: post }).then(updatedPost => {
+    res.json(updatedPost);
+  },
+  error => {
+    res.sendStatus(400);
+  });
+
+}
+// Delete a post
+app.delete('/post/:id', deleteApost);
+
+function deleteApost(req, res) {
+  PostModel.findByIdAndDelete(req.params.id).then(() => {
+    res.sendStatus(200);
+  },
+  error => {
+    res.sendStatus(400);
+  });
+
+}
+
+
+//creat account 
+
+app.post('/createAccount', createAccount);
+
+function createAccount(req, res){
+  bcrypt.hash(req.body.password, 10)
+  .then(password => UserModel.create({ username: req.body.username, password }))
+  .then(userObj => {
+    console.log(userObj);
+    res.sendStatus(200);
+  })
+  .catch(err => {
+    console.error(err);
+    res.sendStatus(400);
+  });
+}
+
+//Bind connection to error event (to get notification of connection errors)
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.listen(8000, () => console.log('Example app listening on port 8000!'))
